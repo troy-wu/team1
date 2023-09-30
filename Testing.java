d13519b16ae48e6ce9f08dec544f198bad6771a5
troywu5 <troywu5@gmail.com>
2023-09-30 11:27:44 -0700

    test with java app

---
+++ Testing.java
@@ -0,0 +1,17 @@
+
+public class Testing {
+    public int var;
+
+    public Testing(int d) {
+        var = d;
+    }
+
+
+
+    public static void main(String[] args) {
+        Testing t = new Testing(20);
+        System.out.println(t.var);
+    }
+}
+
+p
